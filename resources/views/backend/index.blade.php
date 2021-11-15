5dc62e0e2a20e0d7a74269e7aa4b50835d98eb26
coderkhayrul <coderkhayrul@gmail.com>
2021-11-15 17:14:38 +0600

    Get All Pizza On Index Page

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-4">
+        <div class="col-md-2">
             <div class="card">
                 <div class="card-header">Menu</div>
 
@@ -15,11 +15,46 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
                 <div class="card-header">Pizza List</div>
                 <div class="card-body">
+                    <table class="table table-striped">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Image</th>
+                        <th scope="col">Pizza Name</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Category</th>
+                        <th scope="col">S. Price</th>
+                        <th scope="col">M. Price</th>
+                        <th scope="col">L. Price</th>
+                        <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($pizzas as $key => $pizza)
+                        <tr>
+                            <th scope="row">{{ $key+1 }}</th>
+                            <td><img src="{{ Storage::url($pizza->image) }}" alt="Pizza Image" width="50px"></td>
+                            <td>{{ $pizza->name }}</td>
+                            <td>{{ $pizza->description }}</td>
+                            <td>{{ $pizza->category }}</td>
+                            <td>{{ $pizza->small_pizza_price }}</td>
+                            <td>{{ $pizza->medium_pizza_price }}</td>
+                            <td>{{ $pizza->large_pizza_price }}</td>
+                            <td>
+                                <a href="" class="btn btn-primary btn-sm">Edit</a>
+                                <a href="" class="btn btn-danger btn-sm">Delete</a>
+                            </td>
+                        </tr>
+                        @empty
 
+                        @endforelse
+
+                    </tbody>
+                    </table>
                 </div>
             </div>
         </div>
